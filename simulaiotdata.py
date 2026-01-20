407b4bb7c6fbb75e5847c79abf6bf26a44b96ee1
Denis <150442493+Denis0160@users.noreply.github.com>
2026-01-20 18:53:45 +0100

    codice

--- simulaiotdata.py
+++ simulaiotdata.py
@@ -1,67 +1,95 @@
-# Importa il modulo per gestire i file json esterni
-import json
-# Per sleep()
-import time
-# Simulazione del sensore di temperatura
-import misurazione
+import json # Importa il modulo per gestire i file json esterni
+import time #importa il modulo per gestire le pause (sleep)
+import misurazione #importa il modulo che simula il sensore di temperatura
 
-# Caricamento dei parametri
+
+#funzione per caricare i parametri dal file di configurazione
 def caricamento_parametri(fileName: str):
-    # Apertura file
-    with open(fileName) as json_file:
-        # Caricamento dati
-        data = json.load(json_file)
-        # Chiusura file
-        json_file.close()
-        # Ritorno dati
+   
+    try:
+        with open(fileName) as json_file: #Apertura del file in modalità lettura
+            data = json.load(json_file)  #Legge il file e salva i dati in una variabile utilizzabile nel programma
+            
+    #Eccezione se il file non esiste    
+    except FileNotFoundError as e:
+        print("Errore: file non trovato.", e)
+    
+    #Eccezione se il JSON è scritto male
+    except json.JSONDecodeError as e:
+        print("Errore sintassi JSON:", e)
+
+    #Eseguito se non ci sono errori
+    else:
         return data
 
-# ID rilevazione incrementale
-rilevazioni = 1
+    #Eseguito sempre
+    finally:
+        print("Operazione caricamento parametri terminata")
+        
 
-# Classe DatoIoT
+rilevazioni = 1 #Variabile globale per ID progressivo delle rilevazioni
+
+#Classe che rappresenta un DatoIoT
 class DatoIoT:
+    
+    #costruttore della classe DatoIoT
     def __init__(self, temperatura: float, umidita: float, cabina: int, ponte: int):
-        # Variabile globale rilevazione
-        global rilevazioni
+        
+        global rilevazioni #Uso della variabile globale per ID rilevazione
 
+        # Inizializzazione attributi
         self.temperatura = temperatura
         self.umidita = umidita
         self.cabina = cabina
         self.ponte = ponte
-        self.dataeora = time.time()
-        self.rilevazione = rilevazioni
+        self.dataeora = time.time() #salva data e ora corrente
+        self.rilevazione = rilevazioni 
 
-        # Incremento ID rilevazione
+        #Incremento ID rilevazione
         rilevazioni += 1
 
+    #Metodo per convertire l'oggetto in formato JSON
     def to_json(self):
         return json.dumps(self.__dict__)
 
-# Loop infinito invio dati
+#Funzione principale di simulazione IoT
 def simulazione_iot():
-    # Caricamento parametri
-    parametri = caricamento_parametri('parametri.conf')
+    try:
+        parametri = caricamento_parametri('parametri.conf') #Caricamento parametri
 
-    # Loop infinito lettura
-    while True:
-        # Lettura temperatura e umidità
-        temperatura = misurazione.on_temperatura(parametri["N_DECIMALI"])
-        umidita = misurazione.on_umidita(parametri["N_DECIMALI"])
-        # Creazione dato IoT
-        dato = DatoIoT(temperatura, umidita, parametri["CABINA"], parametri["PONTE"])
-        # Stampa dato IoT in formato JSON
-        print(dato.to_json())
-        
-        # Stampa del dato all'interno del file
-        with open('iotdata.dbt', "a") as file_output:
-            file_output.write(dato.to_json() + "\n")
-            file_output.close()
+        #Loop infinito lettura
+        while True:
+            try:
+                temperatura = misurazione.on_temperatura(parametri["N_DECIMALI"])  #Lettura temperatura
+                umidita = misurazione.on_umidita(parametri["N_DECIMALI"]) #Lettura umidità
+                dato = DatoIoT(temperatura, umidita, parametri["CABINA"], parametri["PONTE"]) #Creazione oggetto dato IoT
 
-        
-        # Attesa di tempo rilevazione
-        time.sleep(parametri["TEMPO_RILEVAZIONE"])
+                print(dato.to_json())#Stampa JSON
 
-# Main programma
+                #Scrittura su file
+                with open('iotdata.dbt', "a") as file_output:
+                    file_output.write(dato.to_json() + "\n")
+
+            #Errore nella lettura o scrittura file
+            except IOError as e:
+                print("Errore nella lettura o scrittura file:", e)
+
+            #Altri errori generici
+            except Exception as e:
+                print("Errore generico:", e)
+
+            #Attesa
+            time.sleep(parametri["TEMPO_RILEVAZIONE"])
+
+    #Gestione CTRL+C
+    except KeyboardInterrupt:
+        print("\nTerminazione del programma")
+
+    #Eseguito sempre
+    finally:
+        print("Programma terminato correttamente")
+
+
+#Main programma
 if __name__ == "__main__":
     simulazione_iot()

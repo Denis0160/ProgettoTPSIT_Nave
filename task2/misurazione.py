84a61b477e8d304c41d862f70eb1386f34b6b7ab
Denis0160 <denis.sescu@iisviolamarchesini.edu.it>
2026-01-29 13:18:03 +0100

    parametri

--- task2/misurazione.py
+++ task2/misurazione.py
@@ -11,12 +11,13 @@ import random   # Generazione numeri casuali
 #
 # Funzioni
 #
-def on_temperatura(N):
-    TEMP = round(random.uniform(10,40), N)
+def on_temperatura(min: int, max: int, errore: int):
+    TEMP =(random.uniform(min,max)) + random.uniform(-errore,errore)
+    TEMP = round(TEMP, 2)
     return TEMP
 # Simulazione sensore umidità, da 20 a 90 gradi
 # cifre decimali pari a N
-def on_umidita(N):
-    UMID = round(random.uniform(20,90), N)
+def on_umidita(min: int, max: int, errore: int):
+    UMID =(random.uniform(min,max)) + random.uniform(-errore,errore)
+    UMID = round(UMID, 2)
     return UMID
-#
